795dad220225d42b4623ce8c2e02d95f7cabe4aa
Nikita Elsakov <vocasle@outlook.com>
2023-03-05 21:14:55 +0200

    Create shaders and input layout

--- src/device_resources.rs
+++ src/device_resources.rs
@@ -1,26 +1,34 @@
 pub mod device_resources {
-    use windows::Win32::{
-        Foundation::{BOOL, HWND},
-        Graphics::{
-            Direct3D::D3D_DRIVER_TYPE_HARDWARE,
-            Direct3D11::{
-                D3D11CreateDevice, ID3D11DepthStencilView, ID3D11Device, ID3D11DeviceContext,
-                ID3D11RenderTargetView, ID3D11Texture2D, D3D11_BIND_DEPTH_STENCIL,
-                D3D11_CPU_ACCESS_FLAG, D3D11_CREATE_DEVICE_BGRA_SUPPORT, D3D11_CREATE_DEVICE_DEBUG,
-                D3D11_DEPTH_STENCIL_VIEW_DESC, D3D11_DEPTH_STENCIL_VIEW_DESC_0,
-                D3D11_DSV_DIMENSION_TEXTURE2D, D3D11_RENDER_TARGET_VIEW_DESC,
-                D3D11_RENDER_TARGET_VIEW_DESC_0, D3D11_RESOURCE_MISC_FLAG,
-                D3D11_RTV_DIMENSION_TEXTURE2D, D3D11_SDK_VERSION, D3D11_TEXTURE2D_DESC,
-                D3D11_USAGE_DEFAULT, D3D11_VIEWPORT,
-            },
-            Dxgi::{
-                Common::{
-                    DXGI_ALPHA_MODE_IGNORE, DXGI_FORMAT_B8G8R8A8_UNORM,
-                    DXGI_FORMAT_D24_UNORM_S8_UINT, DXGI_SAMPLE_DESC,
+    use std::fs::OpenOptions;
+
+    use windows::{
+        s,
+        Win32::{
+            Foundation::{BOOL, HWND},
+            Graphics::{
+                Direct3D::D3D_DRIVER_TYPE_HARDWARE,
+                Direct3D11::{
+                    D3D11CreateDevice, ID3D11DepthStencilView, ID3D11Device, ID3D11DeviceContext,
+                    ID3D11InputLayout, ID3D11PixelShader, ID3D11RenderTargetView, ID3D11Texture2D,
+                    ID3D11VertexShader, D3D11_BIND_DEPTH_STENCIL, D3D11_CPU_ACCESS_FLAG,
+                    D3D11_CREATE_DEVICE_BGRA_SUPPORT, D3D11_CREATE_DEVICE_DEBUG,
+                    D3D11_DEPTH_STENCIL_VIEW_DESC, D3D11_DEPTH_STENCIL_VIEW_DESC_0,
+                    D3D11_DSV_DIMENSION_TEXTURE2D, D3D11_INPUT_ELEMENT_DESC,
+                    D3D11_INPUT_PER_VERTEX_DATA, D3D11_RENDER_TARGET_VIEW_DESC,
+                    D3D11_RENDER_TARGET_VIEW_DESC_0, D3D11_RESOURCE_MISC_FLAG,
+                    D3D11_RTV_DIMENSION_TEXTURE2D, D3D11_SDK_VERSION, D3D11_TEXTURE2D_DESC,
+                    D3D11_USAGE_DEFAULT, D3D11_VIEWPORT,
                 },
-                CreateDXGIFactory2, IDXGIFactory7, IDXGISwapChain1, DXGI_CREATE_FACTORY_DEBUG,
-                DXGI_SCALING_NONE, DXGI_SWAP_CHAIN_DESC1, DXGI_SWAP_EFFECT_FLIP_DISCARD,
-                DXGI_USAGE_RENDER_TARGET_OUTPUT,
+                Dxgi::{
+                    Common::{
+                        DXGI_ALPHA_MODE_IGNORE, DXGI_FORMAT_B8G8R8A8_UNORM,
+                        DXGI_FORMAT_D24_UNORM_S8_UINT, DXGI_FORMAT_R32G32B32_FLOAT,
+                        DXGI_SAMPLE_DESC,
+                    },
+                    CreateDXGIFactory2, IDXGIFactory7, IDXGISwapChain1, DXGI_CREATE_FACTORY_DEBUG,
+                    DXGI_SCALING_NONE, DXGI_SWAP_CHAIN_DESC1, DXGI_SWAP_EFFECT_FLIP_DISCARD,
+                    DXGI_USAGE_RENDER_TARGET_OUTPUT,
+                },
             },
         },
     };
@@ -38,6 +46,9 @@ pub mod device_resources {
         pub viewport: D3D11_VIEWPORT,
         pub dsv: ID3D11DepthStencilView,
         pub rtv: ID3D11RenderTargetView,
+        pub vs: ID3D11VertexShader,
+        pub ps: ID3D11PixelShader,
+        pub il: ID3D11InputLayout,
     }
 
     impl DeviceResources {
@@ -141,6 +152,33 @@ pub mod device_resources {
                 )?;
             }
 
+            let mut vs = None;
+            let mut il = None;
+
+            let cwd = std::env::current_dir().unwrap().join("target").join("debug");
+
+            unsafe {
+                let bytes = std::fs::read(cwd.join("vs.cso")).unwrap();
+                device.CreateVertexShader(&bytes as _, None, Some(&mut vs))?;
+
+                let input_desc = vec![D3D11_INPUT_ELEMENT_DESC {
+                    SemanticName: s!("POSITION"),
+                    SemanticIndex: 0,
+                    Format: DXGI_FORMAT_R32G32B32_FLOAT,
+                    InputSlot: 0,
+                    AlignedByteOffset: 0,
+                    InputSlotClass: D3D11_INPUT_PER_VERTEX_DATA,
+                    InstanceDataStepRate: 0,
+                }];
+
+                device.CreateInputLayout(&input_desc, &bytes, Some(&mut il))?;
+            }
+            let mut ps = None;
+            unsafe {
+                let bytes = std::fs::read(cwd.join("ps.cso")).unwrap();
+                device.CreatePixelShader(&bytes as _, None, Some(&mut ps))?;
+            }
+
             return Ok(DeviceResources {
                 device: device,
                 context: context,
@@ -149,6 +187,9 @@ pub mod device_resources {
                 viewport: viewport,
                 dsv: dsv.unwrap(),
                 rtv: rtv.unwrap(),
+                vs: vs.unwrap(),
+                ps: ps.unwrap(),
+                il: il.unwrap(),
             });
         }
     }
